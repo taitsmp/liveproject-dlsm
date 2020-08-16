049f5285c050f69bfd438325f9b8502f11b1f5a3
Tait Larson <telarson@gmail.com>
2020-08-16 16:09:17 -0400

    refactored AllenNLP classes to utils.py

---
+++ utils.py
@@ -0,0 +1,40 @@
+from allennlp.models import Model
+from allennlp.modules.seq2vec_encoders import Seq2VecEncoder, PytorchSeq2VecWrapper
+from allennlp.data.tokenizers.character_tokenizer import CharacterTokenizer
+from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
+from allennlp.data.tokenizers import Token
+from allennlp.data.dataset_readers.dataset_reader import DatasetReader
+from allennlp.data.fields import TextField
+from allennlp.data.instance import Instance
+
+import pandas as pd
+
+
+from torch.nn import LSTM
+
+from typing import Dict, Iterable, Union, Optional, List
+
+@DatasetReader.register("char_lang_mod")
+class CharDatasetReader(DatasetReader):
+    def __init__(self) -> None:
+        
+        #todo: could become args
+        self._token_indexers = {'tokens': SingleIdTokenIndexer()}
+        self._tokenizer = CharacterTokenizer()
+        
+
+    def text_to_instance(self, sentence: str,) -> Instance:
+        
+        tokenized = self._tokenizer.tokenize(sentence)
+        #TODO: add start and end characters
+        #TODO: do you want to add "source" and "target" here? 
+        instance = Instance({"source": TextField(tokenized, self._token_indexers)})
+        return instance
+    
+    def _read(self, df: pd.DataFrame) -> Iterable[Instance]:
+        
+        for row in df.itertuples(index=False):
+            instance = self.text_to_instance(row.text)
+            yield instance
+
+
